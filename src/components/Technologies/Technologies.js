424c58a86e300f5e57911610d1428743e29a53cd
jeffmlb91 <jeffmlb91@gmail.com>
2022-03-13 19:45:58 -0400

    update technologies

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,83 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import {
+  DiFirebase,
+  DiReact,
+  DiZend,
+  DiAngularSimple,
+  DiJavascript,
+  DiJqueryLogo,
+  DiNodejsSmall,
+  DiMongodb,
+} from "react-icons/di";
+import { CgFigma } from "react-icons/cg";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <br/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of technologies from front end to back as well as
+      design.
+    </SectionText>
+    <List>
+      <ListItem>
+        <view style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <DiReact size="3rem" />
+          <DiAngularSimple size="3rem" />
+          <DiJavascript size="3rem" />
+          <DiJqueryLogo size="3rem" />
+        </view>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js, ReactAngular JavaScript & JQuery
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <view style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <DiFirebase size="3rem" />
+          <DiNodejsSmall size="3rem" />
+          <DiMongodb size="3rem" />
+        </view>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <view style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <DiZend size="3rem" />
+          <CgFigma size="3rem" />
+        </view>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Tools like Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
