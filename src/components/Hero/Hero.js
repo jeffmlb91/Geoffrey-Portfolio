4f88440c66c676830c331e7afabc2a06a8549844
jeffmlb91 <jeffmlb91@gmail.com>
2022-03-13 13:57:07 -0400

    update Peroject section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,20 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi There! I'm <br />
+        Geoffrey Moluba <br />
+        Software Engineer Full Stack
+      </SectionTitle>
+      <SectionText>
+      Count me in among your team of passionate developers who are striving to integrate innovative technologies to solve problems and improve upon existing method.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
